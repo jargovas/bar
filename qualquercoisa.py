57c85607c71774e2754af78a3cb5ce9e3018c26f
Unknown <jargovas@gmail.com>
2018-09-21 15:27:31 -0300

    Inicio do Tkinter (fala do professor)

---
+++ qualquercoisa.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+root = tk.Tk()
+tk.Label(root, text= "Bar do Zé Atomatik").pack()
+tk.Button(root, text= "Olha eu sou un button").pack()
+tk.Entry(root).pack()
+root.mainloop()
